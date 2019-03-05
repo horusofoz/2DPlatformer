9336009e066b25e854d4a61d3f2c22313b66a870
Volktenen <48009585+Volktenen@users.noreply.github.com>
2019-03-05 19:51:09 +1030

    CameraController
    for main camera

---
+++ 2DPlatformer/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    //attach to main camera
+    public Transform player;
+    public Vector3 offset;
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); //set offset values to x=0 y=3 z=-10
+	}
+}
