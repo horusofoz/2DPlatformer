6fe4e84af5d2504fac8a370fc720675fae52d96c
Jeff <horusofoz@gmail.com>
2019-03-11 12:51:44 +1030

    * Added enemy tag * Applied enemy tag to skeleton in jeff test scene * Modified weapon attack script to destroy collided game object with enemy tag

--- 2DPlatformer/Assets/Scripts/WeaponAttack.cs
+++ 2DPlatformer/Assets/Scripts/WeaponAttack.cs
@@ -17,5 +17,11 @@ public class WeaponAttack : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log("I hit a " + collider.gameObject.name);
+        if (collider.gameObject.tag == "enemy")
+        {
+            // TODO Call function for enemy hit/die animation
+            // TODO Call function for player animation from successful hit e.g. SFX,VFX
+            Destroy(collider.gameObject);
+        }
     }
 }
