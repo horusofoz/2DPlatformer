1746e053c6b460eba5c480275f34fe65473a4bed
chedster345 <48009548+chedster345@users.noreply.github.com>
2019-05-07 18:02:18 +0930

    Fall Block Created
    The fall block has been made and placed in the environment prefabs.
    
    - You can adjust delay and fall speed (it is not physics based).
    - The collider must be set to the parent object.
    - The children are purely for aesthetic/visibility.

---
+++ 2DPlatformer/Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour {
+	enum FallBlockState
+	{
+		Idle,
+		FallDelay,
+		Falling,
+	}
+	FallBlockState _state = FallBlockState.Idle;
+	public float FallDelay = 2f;
+	public float FallSpeed = 7f;
+
+	private void Update(){
+		switch (_state)
+		{
+			case FallBlockState.FallDelay:
+				FallDelay -= Time.deltaTime;
+				if(FallDelay <= 0f){
+					_state = FallBlockState.Falling;
+				}
+				break;
+			case FallBlockState.Falling:
+				transform.position += Vector3.down * FallSpeed * Time.deltaTime;
+				break;
+		}
+	}
+
+	private void OnBecameInvisible(){
+		if (_state != FallBlockState.Idle) Destroy(gameObject);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision){
+		if(_state == FallBlockState.Idle)
+			_state = FallBlockState.FallDelay;
+	}
+}
