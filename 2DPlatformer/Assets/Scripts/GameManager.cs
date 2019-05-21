3933f9550b9f79d5d1b019048c7a5a52f71ef198
chedster345 <48009548+chedster345@users.noreply.github.com>
2019-05-21 10:00:36 +0930

    Update GameManager.cs
    - New key/door mechanics allow for multiple doors. Other events can be added to the on key get or on door touch to change the world (eg respawn enemies or create/enable new ones).
    
    Keep in mind that the key does not automatically despawn nor does the door automatically despawn on touch after the key is obtained. Please read the notes and check out my level for examples.

--- 2DPlatformer/Assets/Scripts/GameManager.cs
+++ 2DPlatformer/Assets/Scripts/GameManager.cs
@@ -1,17 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance = null;
+	public KeyDoorPair[] KeyDoors;
     private int score;
     private int health;
     private bool keyCollected;
     private bool doubleJumpEnabled;
+	[System.Serializable]
+	public class KeyDoorPair
+	{
+		public bool KeyGot = false;
+		public GameObject Key;
+		[Tooltip("Sets keygot to true automatically but doesn't despawn the door. ")]
+		public UnityEvent OnKeyTouch;
+		public GameObject Door;
+		[Tooltip("Only runs if keygot is true. ")]
+		public UnityEvent OnDoorTouch;
 
+		public void SetKeyDoors()
+		{
+			var keyScript = Key.AddComponent(typeof(KeyScript)) as KeyScript;
+			var doorScript = Door.AddComponent(typeof(DoorScript)) as DoorScript;
+			keyScript.thisPair = this;
+			doorScript.thisPair = this;
+		}
+
+		public class KeyScript : MonoBehaviour
+		{
+			[HideInInspector] public KeyDoorPair thisPair;
+			private void OnCollisionEnter2D(Collision2D collision)
+			{
+				OnTriggerEnter2D(collision.collider);
+			}
+			private void OnTriggerEnter2D(Collider2D collision)
+			{
+				if (collision.gameObject.tag == "Player")
+				{
+					thisPair.KeyGot = true;
+					thisPair.OnKeyTouch.Invoke();
+				}
+			}
+		}
+		public class DoorScript : MonoBehaviour
+		{
+			[HideInInspector] public KeyDoorPair thisPair;
+			private void OnCollisionEnter2D(Collision2D collision)
+			{
+				OnTriggerEnter2D(collision.collider);
+			}
+			private void OnTriggerEnter2D(Collider2D collision)
+			{
+				if (collision.gameObject.tag == "Player" && thisPair.KeyGot)
+				{
+					thisPair.OnDoorTouch.Invoke();
+				}
+			}
+		}
+	}
     private void Awake()
     {
+		foreach (var keydoorpair in KeyDoors)
+		{
+			keydoorpair.SetKeyDoors();
+		}
         if(instance == null)
         {
             instance = this;
